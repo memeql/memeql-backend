2ddcf8e81344444ec5a60051b0b2cf9233b770ad
Konstantin <konstantin.kaminskiy@outlook.com>
2024-07-22 18:35:05 -0400

    adds get, update, delete for memes

--- routes/memesRoutes.js
+++ routes/memesRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { memesCtrl } = require('../controllers')
 
+router.get('/', memesCtrl.getMemes)
 router.post('/', memesCtrl.createMeme)
+router.put('/:id', memesCtrl.updateMeme)
+router.delete('/:id', memesCtrl.deleteMeme)
 
 module.exports = router

50e7dd6f0821b70057d33f2d59cd4c9352681001
Konstantin <konstantin.kaminskiy@outlook.com>
2024-07-26 21:12:21 -0400

    change user validation to a get request so it works from browser

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,6 @@ const { authCtrl } = require('../controllers')
 
 router.post('/', authCtrl.registerUser)
 router.post('/login', authCtrl.loginUser)
-router.post('/userValidation/:userId', authCtrl.validateUser)
+router.get('/userValidation/:userId', authCtrl.validateUser)
 
 module.exports = router
